f68627b8cfcea44a2943d345b7957e95437f0387
Gonzalez Alayon <dgonzalezalayon@ga.afginc.com>
2017-12-26 16:21:13 -0600

    added validation to email and name

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { states } from '../data-model';
+import{Observable} from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,10 +9,65 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  name = new FormControl();
-  constructor() { }
-
+  heroForm: FormGroup; // <--- heroForm is of type FormGroup
+  states = states;
+  password = "";
+  constructor(private fb: FormBuilder) { // <--- inject FormBuilder
+    this.createForm();
+  }
   ngOnInit() {
   }
 
+  createForm() {
+    this.heroForm = this.fb.group({ // <-- the parent FormGroup
+      name: ['', Validators.compose([
+        Validators.required, 
+        Validators.maxLength(10),
+        Validators.minLength(4)
+      ])],
+      email: ['', Validators.compose([
+        Validators.required, 
+        Validators.email,
+      ])],
+
+      email2: ['', Validators.compose([
+        Validators.required, 
+        Validators.email,]),
+        this.validPassword.bind(this)
+       ],
+      address: this.fb.group({ // <-- the child FormGroup
+        street: '',
+        city: '',
+        state: '',
+        zip: ['', Validators.compose([
+          Validators.required,           
+          Validators.maxLength(5),
+          Validators.minLength(5)
+        ])],
+      }),
+      power: '',
+      sidekick: ''
+    });
+  }
+
+  clicked(){    
+    console.log(this.heroForm.controls.address);
+
+  }
+  validPassword(control: AbstractControl) {            
+    return Observable
+         .of(this.password === control.value)
+         .map(result => !!result ? null : { invalid: true });        
+  }
+  
+
 }
+/*
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  };
+}
+*/

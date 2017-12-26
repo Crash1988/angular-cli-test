a369241a2dcf3988000cf4d65c2aea7a7cf82609
Gonzalez Alayon <dgonzalezalayon@ga.afginc.com>
2017-12-26 13:48:44 -0600

    adding form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AlertModule } from 'ngx-bootstrap'; 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HeroDetailComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [AlertModule.forRoot(),
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
